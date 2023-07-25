05d2ef7b1223e9ec0dd2c99a9be0c1d44e7e94e1
Chen Liang <chen.liang@ie.ibm.com>
2023-07-25 20:36:41 +0100

    Create new() and add() function

--- linked_list_rust/single_linked_list/src/main.rs
+++ linked_list_rust/single_linked_list/src/main.rs
@@ -1,15 +1,36 @@
+#[derive(Debug)]
+#[allow(dead_code)]
 struct Node<T: std::fmt::Debug + std::marker::Copy> {
     element: T,
-    next: Box<Option<Node<T>>>,
-}
-struct LinktedList<T: std::fmt::Debug + std::marker::Copy> {
-    head: Box<Option<Node<T>>>,
+    next: Pointer<T>,
 }
 
-impl<T: std::fmt::Debug + std::marker::Copy> LinktedList<T> {
-    // TODO: linked list methods
+type Pointer<T> = Option<Box<Node<T>>>;
+
+impl<T: std::fmt::Debug + std::marker::Copy> Node<T> {
+    fn new(data: T) -> Node<T> {
+        Node {
+            next: None,
+            element: data,
+        }
+    }
+
+    fn add(&mut self, data: T) {
+        let element = self.element;
+        let next = self.next.take();
+
+        let previous_pointer = Box::new(Node { element, next });
+        self.element = data;
+        self.next = Some(previous_pointer);
+    }
+
+    // fn display_list(&self) {
+    //
+    // }
 }
 
 fn main() {
-
+    let mut single_linked_list = Node::new(3);
+    single_linked_list.add(4);
+    println!("{:?}", single_linked_list);
 }
