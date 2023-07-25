ac617f3bb7b89fab982bf23c539fc522d5ce7821
Chen Liang <chen.liang@ie.ibm.com>
2023-07-25 17:02:45 +0100

    Intial linked list struct

--- linked_list_rust/single_linked_list/src/main.rs
+++ linked_list_rust/single_linked_list/src/main.rs
@@ -1,3 +1,15 @@
+struct Node<T: std::fmt::Debug + std::marker::Copy> {
+    element: T,
+    next: Box<Option<Node<T>>>,
+}
+struct LinktedList<T: std::fmt::Debug + std::marker::Copy> {
+    head: Box<Option<Node<T>>>,
+}
+
+impl<T: std::fmt::Debug + std::marker::Copy> LinktedList<T> {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+
 }
