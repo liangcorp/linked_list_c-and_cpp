1aedffdffec3f58d5ae4e19a82a7ce6e22df9b18
Chen Liang <chen.liang@ie.ibm.com>
2024-02-03 11:42:31 +0000

    single linked list in zig

---
+++ linked_list_zig/single_linked_list.zig
@@ -0,0 +1,37 @@
+const std = @import("std");
+
+const Node = struct {
+    x: i32,
+    next: ?*Node = null,
+};
+
+pub fn add_last(head: *Node, x: i32) !void {
+    var new_node = Node{
+        .x = x,
+        .next = null,
+    };
+
+    var node = head;
+    while (node.next != null) {
+        node = node.next.?;
+    }
+
+    node.next = &new_node;
+}
+
+pub fn display(head: *Node) !void {
+    var node = head;
+    while (node.next != null) {
+        std.debug.print("{}\n", .{node.x});
+        node = node.next.?;
+    }
+}
+pub fn main() !void {
+    var head = Node{ .x = 9, .next = null };
+    try add_last(&head, 10);
+    try add_last(&head, 11);
+    try add_last(&head, 12);
+    try add_last(&head, 12);
+
+    try display(&head);
+}
